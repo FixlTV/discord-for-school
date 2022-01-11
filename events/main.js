e7f8b53f64d7c8e688592b1a7f78951b9818aa75
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-01-11 11:36:13 +0100

    automatic deletion of past data

--- events/main.js
+++ events/main.js
@@ -1,17 +1,22 @@
-const discord   = require('discord.js')
+const fs = require('fs')
 
 module.exports = {
     name: 'Infonachricht',
     event: 'ready',
     once: true,
     async on(client) {
-        let tempdate = new Date()
-        var dold = new Date(tempdate.setMinutes(59))
+        var dold = new Date()
         setInterval(async () => {
             const config = require('../config.json')
             const d = new Date()
             if((d.getHours() == config.sendtime && (d.getMinutes() < dold.getMinutes()))) {
                 global.events.emit('editMessage')
+            }
+            if(dold.getDate() != d.getDate()) {
+                let test = require('../test.json')
+                let ha = require('../ha.json')
+                if(test[dold.getMonth()]?.[dold.getDate()]) delete test[dold.getMonth()]?.[dold.getDate()]
+                if(ha[dold.getMonth()]?.[dold.getDate()]) delete ha[dold.getMonth()]?.[dold.getDate()]
             }
             dold = new Date()
         }, 30000, dold)
