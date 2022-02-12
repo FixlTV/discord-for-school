5f32abe0485916fce1460b0c47cc9661c4aed858
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-02-12 13:12:43 +0100

    setup fix 2

--- setup.js
+++ setup.js
@@ -213,7 +213,7 @@ module.exports = async () => {
         }
         for await (let key of require('./config.template.json')) {
             if(!config[key]) {
-                if(!changed) {
+                if(!changed && changed != false) {
                     console.log('\x1b[91m%s\x1b[0m', '[X]', 'Config unvollständig!')
                     changed = true
                 }
