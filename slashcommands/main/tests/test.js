3d3f1651bebd6ffd874402dcac7b8c00c9b20d20
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-02-19 10:22:43 +0100

    Gelöschte Test Events löschen

--- slashcommands/main/tests/test.js
+++ slashcommands/main/tests/test.js
@@ -139,7 +139,7 @@ module.exports = {
                 if(args.uhrzeit.value > 24 || args.uhrzeit.value < 0) return await warn(ita, 'Syntaxfehler', `\`Uhrzeit\` muss eine Zahl zwischen 0 und 24 sein.\nDas Event wird nicht erstellt; der ${subject} Test (${testtype}) am ${day}.${month}. wurde gespeichert.`)
                 if(args.dauer.value < 1 || args.dauer.value > 7) return await warn(ita, 'Syntaxfehler', `\`Dauer\` muss eine Zahl zwischen 1 und 7 sein.\nDas Event wird nicht erstellt; der ${subject} Test (${testtype}) am ${day}.${month}. wurde gespeichert.`)
                 
-                await guild.scheduledEvents.fetch({ force: true })
+                await guild.scheduledEvents.fetch()
 
                 let eventData = {
                     name: `${subject} | ${testtype}`,
@@ -156,26 +156,47 @@ module.exports = {
                     .filter(e => eventData.name === e.name && eventData.scheduledStartTime.getTime() == e.scheduledStartTimestamp)
                     .length
                 ) {
-                    guild.scheduledEvents.create(eventData)
+                    try { guild.scheduledEvents.create(eventData) } catch {}
                 }
             }
             return success(ita, `Test hinzugefügt`, `Ein ${subject} Test (${testtype}) wurde am ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} hinzugefügt.`)
         } else {
-            var subject = args.fach.value
-            var day = args['tag'].value
-            var month = Number(args['monat'].value) - 1
-            var date = new Date()
+            let subject = args.fach.value
+            let day = args['tag'].value
+            let month = Number(args['monat'].value) - 1
+            let date = new Date()
             if(!(1 <= month <= 12)) return error(ita, 'Syntaxfehler', '`Monat` muss eine Zahl zwischen 1 und 12 sein.')
             date.setMonth(month, day)
             date.setHours(0, 0, 0, 0)
             if(Date.now() >= date.getTime()) date.setFullYear(date.getFullYear() + 1)
-            var test = require('../../../data/test.json')
+            let test = require('../../../data/test.json')
             if(!test[date.getMonth()] || !test[date.getMonth()][date.getDate()] || !test[date.getMonth()][date.getDate()][subject]) return error(ita, 'Fehler', `An diesem Tag wird kein ${subject} Test geschrieben.`)
             let testtype = test[date.getMonth()][date.getDate()][subject]
             delete test[date.getMonth()][date.getDate()][subject]
             if(test[date.getMonth()][date.getDate()] == {}) delete test[date.getMonth()][date.getDate()]
             await fs.writeFile('data/test.json', JSON.stringify(test))
             global.events.emit('editMessage')
+            if(useEvents) {
+                let { guild } = ita
+                await guild.scheduledEvents.fetch()
+                let name = `${subject} | ${testtype}`
+                let events = [...guild.scheduledEvents.cache.values()]            
+                    ?.filter(e => e.creatorId === client.user.id)
+                    ?.map(e => {
+                        let date = new Date(e.scheduledStartTimestamp)
+                        date.setHours(0, 0, 0, 0)
+                        return {
+                            name: e.name,
+                            date,
+                            id: e.id
+                        }
+                    })
+                    ?.filter(e => e.name == name && e.date.getTime() == date.getTime())
+                
+                if(events.length) {
+                    try { guild.scheduledEvents.delete(events[0].id) } catch {}
+                }
+            }
             return success(ita, `Test gelöscht`, `Der ${subject} Test (${testtype}) am ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} wurde gelöscht.`)
         }
     }
